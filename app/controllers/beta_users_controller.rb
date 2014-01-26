0a31d4890329434f3dda820acc3873abf66cca74
Shane Burkhart <shaneburkhart@gmail.com>
2014-01-25 18:33:50 -0600

    Wrong name for mailer view

--- app/controllers/beta_users_controller.rb
+++ app/controllers/beta_users_controller.rb
@@ -3,7 +3,6 @@ class BetaUsersController < ApplicationController
     @beta_user = BetaUser.new params.require(:beta_user).permit(:email, :sign_up_form)
     if @beta_user.save
       UserMailer.welcome_email(@beta_user).deliver
-
       redirect_to root_path, flash: {notice: "Thanks for signing up!  We'll keep you updated."}
     else
       flash[:error] = @beta_user.errors.full_messages.to_sentence
