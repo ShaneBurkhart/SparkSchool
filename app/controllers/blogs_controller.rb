0b588d954936bd03324ba17c22eec59fb66a386c
Shane Burkhart <shaneburkhart@gmail.com>
2014-01-27 11:29:48 -0600

    Blogs controller flow

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,18 +12,40 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
 
+  def create
+    @blog = Blog.new blog_params
+    if @blog.save
+      redirect_to blogs_path, flash: {notice: "Successfully created blog post."}
+    else
+      render "new"
+    end
   end
 
   def edit
-
+    @blog = Blog.find(params[:id])
   end
 
   def destroy
-
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path, flash: {notice: "Successfully deleted blog post."}
   end
 
   def update
-
+    @blog = Blog.find(params[:id])
+    @blog.attributes = blog_params
+    if @blog.save
+      redirect_to blogs_path, flash: {notice: "Successfully updated blog."}
+    else
+      render "edit"
+    end
   end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :body, :tag)
+    end
 end
