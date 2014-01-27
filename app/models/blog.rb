2d460ca4c9c5ff869e6d9e91e0f68d9558ae3226
Shane Burkhart <shaneburkhart@gmail.com>
2014-01-27 12:51:17 -0600

    Link title now added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ActiveRecord::Base
   validates :title, :body, :tag, presence: true
+
+  def link_title
+    return self.title.gsub(/[^a-zA-Z0-9\s\-]/, '').gsub(/\s+/, "-").gsub(/\-+\z/, '').downcase
+  end
 end
