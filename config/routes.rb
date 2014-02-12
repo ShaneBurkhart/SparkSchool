83735ed7cd259e0685958b28f53a994e32790894
Shane Burkhart <shaneburkhart@gmail.com>
2014-02-11 20:46:41 -0600

    Removed beta user path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 SparkAcademy::Application.routes.draw do
   root :to => "home#index"
 
-  resources :beta_users, path: "/users"
-
   #blog routes
   resources :blogs, path: "/blog"
   get "/blog/:id/:title", to: "blogs#show", as: "blog_title"
