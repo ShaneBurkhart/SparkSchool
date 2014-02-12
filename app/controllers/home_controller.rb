d9ae25b0ec3b0735e99e7ed7990067637bddec61
Shane Burkhart <shaneburkhart@gmail.com>
2014-02-11 20:53:16 -0600

    Everything is restored back to normal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    #redirect_to courses_path if current_user
-    @user = BetaUser.new
+    redirect_to courses_path if current_user
   end
 end
