91788bd6d763adb81c2b23465f106f5732687521
Shane Burkhart <shaneburkhart@gmail.com>
2014-02-27 14:33:01 -0600

    Routes modified.  No user except admin so no registration.  Courses only index for public.

---
+++ app/controllers/preorders_controller.rb
@@ -0,0 +1,9 @@
+class PreordersController < ApplicationController
+  def show
+
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,37 @@
 SparkAcademy::Application.routes.draw do
+  get "preorders/show"
+  get "preorders/create"
   root :to => "home#index"
 
-
-  #temporary
-  devise_for :users, skip: [:registrations, :password]
-
-
-
-  resources :beta_users, path: "/users"
+  get "/preorders", to: "preorders#show"
 
   #blog routes
   resources :blogs, path: "/blog"
   get "/blog/:id/:title", to: "blogs#show", as: "blog_title"
   post "/newsletter", to: "newsletter_users#create" # has to go after resource!!!
 
-=begin
   #tutorial routes
   resources :courses, except: ["show"] do
     resources :units, except: ["index", "show"] do
       resources :lessons, except: ["index", "show"]
     end
   end
+=begin
   get "courses/:tag", to: "courses#show", as: "course_tag" #this will be show.  tag is to make it more use friendly
   get "courses/:tag/:lesson_number", #finds course by tag then looks for lesson relative to course
         to: "lessons#show",
         as: "course_tag_lesson",
         constraints: {lesson_number: /\d/} #makes it only integers
 
+=end
 
-  #devise and user routes
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  #resources :users
+  #only admins need this
+  devise_for :users, skip: [:registration, :password]
 
 
   #user homepage.  shows their current progress
   #get "dashboard", to: "dashboard#index"
 
-=end
 
   #private mail url
   post "/_incoming_mail", to: "incoming_mail#parse"
