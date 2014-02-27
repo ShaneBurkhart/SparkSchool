f50187cac0d637022dc3f1f21b2d1fc29809cabb
Shane Burkhart <shaneburkhart@gmail.com>
2014-02-27 17:06:26 -0600

    Messages for invalid preorder

--- app/controllers/preorders_controller.rb
+++ app/controllers/preorders_controller.rb
@@ -4,6 +4,27 @@ class PreordersController < ApplicationController
   end
 
   def create
-
+    @preorder = Preorder.new preorder_params
+    if @preorder.save
+      redirect_to preorder_path, flash: { notice: "Your order has been processed.  Thanks!" }
+    else
+      flash[:error] = "Something went wrong."
+      render "show"
+    end
   end
+
+  private
+
+    def preorder_params
+      params.require(:preorder).permit(
+        :first_name,
+        :last_name,
+        :email,
+        :address,
+        :city,
+        :state,
+        :zipcode,
+        :strip_customer_id #Not right but this is for you Ryan.
+      )
+    end
 end
