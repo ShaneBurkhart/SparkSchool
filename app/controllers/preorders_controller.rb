646cfd4541f1fbfab6d259d3842e2bf3a8d22bec
Ryan Endacott <rzeg24@gmail.com>
2014-03-02 17:03:02 -0600

    preorder controller done but untested

--- app/controllers/preorders_controller.rb
+++ app/controllers/preorders_controller.rb
@@ -4,13 +4,35 @@ class PreordersController < ApplicationController
   end
 
   def create
+
     @preorder = Preorder.new preorder_params
-    if @preorder.save
-      redirect_to preorder_path, flash: { notice: "Your order has been processed.  Thanks!" }
-    else
+    unless @preorder.save
       flash[:error] = "Something went wrong."
       render "show"
     end
+
+    # in cents
+    @amount = 12900
+
+    customer = Stripe::Customer.create(
+      email: params[:email],
+      card: 'thisisabadtoken' # params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: "Kit preorder from #{params[:first_name]} #{params[:last_name]}",
+      currency: 'usd'
+    )
+
+
+    redirect_to preorder_path, flash: { notice: "Your order has been processed.  Thanks!" }
+
+  rescue Stripe::CardError => e
+    @preorder.delete # delete the preorder
+    flash[:error] = e.message
+    render "show"
   end
 
   private
