cee6e9a81df7f758e5d77dd05488341534d8eae7
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-02 01:51:14 -0600

    Add rendering static jekyll pages.

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+function PageNotFoundError(path) {
+  this.name = 'PageNotFoundError';
+  this.message = 'Couldn\'t find page: ' + path;
+}
+
+app.engine('html', require('pug').renderFile);
+
+app.get('/', function(req, res) {
+  res.send('Hello world!');
+});
+
+app.get('/courses/*', function(req, res, next) {
+  var filePath = __dirname + '/courses/_site' + req.path + '.html';
+
+  res.sendFile(filePath, function(err) {
+    if (err) next(new PageNotFoundError(req.path));
+  });
+});
+
+app.use(function(err, req, res, next) {
+  console.log(err.message);
+
+  switch(err.name) {
+    case 'PageNotFoundError':
+      return res.status(404).send(err.message);
+    default:
+      res.status(500).send('There was an internal error.');
+  }
+});
+
+app.listen(8080, function() {
+  console.log('Listening on port 8080');
+});
