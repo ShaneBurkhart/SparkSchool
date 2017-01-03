ced10ac7d39da1c49271cad8f53697af6a56f04e
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-02 22:52:20 -0600

    Add user logins and signups to authenticated /courses routes.

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,50 @@
 'use strict'
 
-module.exports = function(app) {
+var User = require('../models/user');
+
+function login(res, userId) {
+  res.cookie('ssuid', userId, { httpOnly: true });
+}
+
+module.exports = function (app) {
+  app.get('/login', function (req, res) {
+    var email = req.query.email || '';
+    var error = req.query.error || '';
+
+    res.render('user/login', { email: email, error: error });
+  });
+
+  app.post('/user/session/create', function (req, res) {
+    var user = { email: req.body.email };
+    var password = req.body.password;
+
+    User.checkPassword(user, password, function (err, user) {
+      if (err) return res.render('user/login', { user: user, error: err });
+
+      login(res, user.id);
+
+      return res.redirect('/courses');
+    });
+  });
+
+  app.get('/signup', function (req, res) {
+    res.render('user/new');
+  });
+
+  app.post('/user/create', function (req, res) {
+    var user = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    User.create(user, function (err, id) {
+      if (err) return res.render('user/new', { user: user, error: err });
+
+      login(res, id);
+
+      res.redirect('/courses');
+    });
+  });
 };
---
+++ models/user.js
@@ -0,0 +1,80 @@
+'use strict'
+
+var bcrypt = require('bcrypt');
+var db = require('../db/db');
+
+// Bcrypt stuff
+var SALT_ROUNDS = 10;
+
+var FIND_BY_ID_QUERY= 'SELECT * FROM Users WHERE id = $1';
+var FIND_BY_EMAIL_QUERY= 'SELECT * FROM Users WHERE email = $1';
+
+var INSERT_QUERY = [
+  'INSERT INTO Users(',
+    'first_name, last_name, email, password_digest',
+  ') VALUES ($1, $2, $3, $4) RETURNING id',
+].join(' ');
+
+module.exports = {
+  // Don't do password validation in here.
+  validate: function (user, callback) {
+    if (!user.first_name) return callback('You must enter a first name.');
+    if (!user.last_name) return callback('You must enter a last name.');
+    if (!user.email) return callback('You must enter an email address.');
+
+    if (!/\S+@\S+.\S+/.test(user.email)) {
+      return callback('You must enter a valid email address.');
+    }
+
+    this.findByEmail(user.email, function (err, user) {
+      if (err) return callback('Sorry, there was an error. Try again later.');
+
+      if (user) return callback('That email is already taken.');
+
+      // Valid user
+      return callback();
+    });
+  },
+
+  create: function (user, callback) {
+    this.validate(user, function (err) {
+      if (err) return callback(err);
+
+      bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+        if (err) return callback('Sorry, there was an error. Try again later.');
+
+        var params = [user.first_name, user.last_name, user.email, hash];
+
+        db.query(INSERT_QUERY, params, function (err, results) {
+          if (err) return callback('Sorry, there was an error. Try again later.');
+          var userId = results.rows[0].id;
+
+          callback(undefined, userId);
+        });
+      });
+    });
+  },
+
+  checkPassword: function (user, password, callback) {
+    this.findByEmail(user.email, function (err, user) {
+      if (err || !user) return callback('Invalid email email and password combination.');
+
+      bcrypt.compare(password, user.password_digest, function (err, res) {
+        if (err || !res) return callback('Invalid email email and password combination.');
+        callback(undefined, user);
+      });
+    });
+  },
+
+  findById: function (id, callback) {
+    db.query(FIND_BY_ID_QUERY, [id], function (err, results) {
+      callback(err, results.rows[0]);
+    });
+  },
+
+  findByEmail: function (email, callback) {
+    db.query(FIND_BY_EMAIL_QUERY, [email], function (err, results) {
+      callback(err, results.rows[0]);
+    });
+  },
+};
