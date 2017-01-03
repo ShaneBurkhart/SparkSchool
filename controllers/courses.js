e0878abef8c0a69bad28f9d33e2ec343006fd4ad
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-03 15:10:29 -0600

    Add auth user middleware and redirect to purchase page if haven't purchases.

--- controllers/courses.js
+++ controllers/courses.js
@@ -5,11 +5,11 @@ var errorUtil = require('../util/error');
 var authUser = require('../middleware/auth-user');
 
 module.exports = function (app) {
-  app.get('/courses', authUser, function (req, res) {
+  app.get('/courses', authUser('paid'), function (req, res) {
     res.render('courses/index');
   });
 
-  app.get('/courses/*', authUser, function (req, res, next) {
+  app.get('/courses/*', authUser('paid'), function (req, res, next) {
     var relativePath = __dirname + '/../courses/_site' + req.path + '.html';
     var absolutePath = path.resolve(relativePath);
 
--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -2,13 +2,24 @@
 
 var User = require('../models/user');
 
-module.exports = function (req, res, next) {
-  var userId = req.cookies.ssuid;
+module.exports = function (role) {
+  return function (req, res, next) {
+    var userId = req.cookies.ssuid;
 
-  if (!userId) return res.redirect('/login');
+    if (!userId) return res.redirect('/login');
 
-  User.findById(userId, function (err, user) {
-    if (!user) return res.redirect('/login');
-    next();
-  });
+    User.findById(userId, function (err, user) {
+      if (!user) return res.redirect('/login');
+
+      if (role) {
+        switch (role) {
+          case 'paid':
+            if (user.status !== 'paid') return res.redirect('/beta/purchase');
+        }
+      }
+
+      req.user = user;
+      next();
+    });
+  };
 };
