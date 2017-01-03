44bbf3dc1c2bfddda42e2e38e51d0eb24ee156d4
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-02 22:21:48 -0600

    Add user auth middleware to course routes.

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,19 +1,20 @@
 'use strict'
 
 var path = require('path');
-
-function PageNotFoundError(path) {
-  this.name = 'PageNotFoundError';
-  this.message = 'Couldn\'t find page: ' + path;
-}
+var errorUtil = require('../util/error');
+var authUser = require('../middleware/auth-user');
 
 module.exports = function (app) {
-  app.get('/courses/*', function (req, res, next) {
+  app.get('/courses', authUser, function (req, res) {
+    res.render('courses/index');
+  });
+
+  app.get('/courses/*', authUser, function (req, res, next) {
     var relativePath = __dirname + '/../courses/_site' + req.path + '.html';
     var absolutePath = path.resolve(relativePath);
 
     res.sendFile(absolutePath, function (err) {
-      if (err) next(new PageNotFoundError(req.path));
+      if (err) next(new errorUtil.PageNotFoundError(req.path));
     });
   });
 };
