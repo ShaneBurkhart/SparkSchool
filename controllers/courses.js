a133b66e2b8a731bf5cd46419b3df1f6b01e4622
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-03 17:45:45 -0600

    Add course name check to courses controller.

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var path = require('path');
+var Course = require('../models/course');
 var errorUtil = require('../util/error');
 var authUser = require('../middleware/auth-user');
 
@@ -9,9 +10,16 @@ module.exports = function (app) {
     res.render('courses/index');
   });
 
-  app.get('/courses/*', authUser('paid'), function (req, res, next) {
-    var relativePath = __dirname + '/../courses/_site' + req.path + '.html';
+  app.get('/courses/:course([\-a-z]+)/:section([0-9]+)/:lesson([0-9]+)', authUser('paid'), function (req, res, next) {
+    var courseName = req.params.course;
+    var sectionNumber = req.params.section;
+    var lessonNumber = req.params.lesson;
+    var relativePath = [__dirname, '..', 'courses', '_site', 'courses', courseName, sectionNumber, lessonNumber].join('/') + '.html';
     var absolutePath = path.resolve(relativePath);
+
+    if (!Course.courseNames.includes(courseName)) {
+      return next(new errorUtil.PageNotFoundError(req.path));
+    }
 
     res.sendFile(absolutePath, function (err) {
       if (err) next(new errorUtil.PageNotFoundError(req.path));
---
+++ models/course.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Course name to id
+var COURSES = {
+  'twitter-clone': 1,
+  'ebay-web-scraper': 2,
+};
+
+module.exports = {
+  COURSES: COURSES,
+  courseNames: Object.keys(COURSES),
+};
