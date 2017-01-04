9e83c9f831425a1748fed6884412ff4fc584339d
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-04 17:12:45 -0600

    Fix typo

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Navigate to directory and pull code
-cd ~/SparkSchool
 git pull origin master
 
 # Build jekyll
-#docker run --rm -v `pwd`:/app ruby bash -c "bundle install --gemfile=/app/Gemfile && jekyll build --source /app/courses --destination /app/courses/_site"
+docker run --rm -v `pwd`:/app ruby bash -c "bundle install --gemfile=/app/Gemfile && jekyll build --source /app/courses --destination /app/courses/_site"
 
 # Build assets and containers
-#docker-compose -f docker-compose.prod.yml build app
-#docker-compose -f docker-compose.dev.yml run app gulp build
-#docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml build app
+docker-compose -f docker-compose.dev.yml run app gulp build
+docker-compose -f docker-compose.prod.yml build
 
 docker-compose -f docker-compose.prod.yml down
-docker-compose -f docker-compose.prod.yml -d up
+docker-compose -f docker-compose.prod.yml up -d
