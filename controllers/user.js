607e23b022d3ec0324546eece7c21978dedfe5f5
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-04 02:05:17 -0600

    Add dynamic price cookie

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,14 @@ var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 var User = require('../models/user');
 var authUser = require('../middleware/auth-user');
 
+var PRICES = {
+  '632f0aa6-5bc9-4fa3-a0f4-d6b08e24638d': 1000,
+  '8079f7a9-0bac-47d9-90df-775fc4c0c9c7': 2000,
+  'bce8d35a-eb30-4bf7-aab5-b6b9e0c89199': 3000,
+  '9cd6fc90-78f8-425f-9f58-dbbaef819a44': 4000,
+  '9b0e60c6-2408-4fb3-a6c2-b7fd3c33a13f': 5000,
+};
+var DEFAULT_PRICE_ID = '632f0aa6-5bc9-4fa3-a0f4-d6b08e24638d';
 
 function login(res, userId) {
   res.cookie('ssuid', userId, { httpOnly: true });
@@ -41,10 +49,22 @@ module.exports = function (app) {
   });
 
   app.get('/signup', function (req, res) {
-    res.render('user/new');
+    // We are using gid to map to price uuids.
+    var gidCookie = req.cookies.ssgid;
+    var gidParam = req.query.gid || DEFAULT_PRICE_ID;
+    var priceInCents = PRICES[gidCookie] || PRICES[DEFAULT_PRICE_ID];
+
+    if (!gidCookie) {
+      res.cookie('ssgid', gidParam, { httpOnly: true });
+      priceInCents = PRICES[gidParam];
+    }
+
+    res.render('user/new', { price: Math.floor(priceInCents / 100) });
   });
 
   app.post('/user/create', function (req, res) {
+    var gidCookie = req.cookies.ssgid || DEFAULT_PRICE_ID;
+    var priceInCents = PRICES[gidCookie] || PRICES[DEFAULT_PRICE_ID];
     var user = {
       full_name: req.body.full_name,
       email: req.body.email,
@@ -52,7 +72,13 @@ module.exports = function (app) {
     };
 
     User.create(user, function (err, id) {
-      if (err) return res.render('user/new', { user: user, error: err });
+      if (err) {
+        return res.render('user/new', {
+          user: user,
+          error: err,
+          price: Math.floor(priceInCents / 100),
+        });
+      }
 
       login(res, id);
 
@@ -61,7 +87,10 @@ module.exports = function (app) {
   });
 
   app.get('/beta/purchase', authUser(), function (req, res) {
-    res.render('user/beta-purchase');
+    var gidCookie = req.cookies.ssgid || DEFAULT_PRICE_ID;
+    var priceInCents = PRICES[gidCookie] || PRICES[DEFAULT_PRICE_ID];
+
+    res.render('user/beta-purchase', { price: Math.floor(priceInCents / 100) });
   });
 
   app.post('/beta/purchase', authUser(), function (req, res) {
