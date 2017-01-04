2f553373503d5d350cae8107946e2edf6f59b7c3
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-04 01:34:42 -0600

    Add twitter clone lead page for home page.

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,15 @@
 'use strict'
 
+var path = require('path');
+var errorUtil = require('../util/error');
+
 module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.render('home/index');
+  app.get('/', function (req, res, next) {
+    var relativePath = [__dirname, '..', 'index.html'].join('/');
+    var absolutePath = path.resolve(relativePath);
+
+    res.sendFile(absolutePath, function (err) {
+      if (err) next(new errorUtil.PageNotFoundError(req.path));
+    });
   });
 };
