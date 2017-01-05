6c8167ce299d2db0e51200299cbc93693dfa24d5
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-04 23:50:27 -0600

    Redirect homepage to signup page.

--- controllers/home.js
+++ controllers/home.js
@@ -5,11 +5,6 @@ var errorUtil = require('../util/error');
 
 module.exports = function (app) {
   app.get('/', function (req, res, next) {
-    var relativePath = [__dirname, '..', 'index.html'].join('/');
-    var absolutePath = path.resolve(relativePath);
-
-    res.sendFile(absolutePath, function (err) {
-      if (err) next(new errorUtil.PageNotFoundError(req.path));
-    });
+    res.redirect('/signup');
   });
 };
