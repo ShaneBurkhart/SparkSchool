aa688ff6ac329f29b190774faf88618e1867be5c
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-20 15:54:27 -0600

    Add some docker clean up code to deploy.

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -13,4 +13,9 @@ docker run -v $(pwd)/public:/app/public sparkschool_app gulp build
 docker-compose -f docker-compose.prod.yml build nginx
 
 docker-compose -f docker-compose.prod.yml down
+# Clean up docker stopped docker containers
+docker rm $(docker ps -aq)
 docker-compose -f docker-compose.prod.yml up -d
+
+# Remove untagged docker images
+docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
