69a6c0bf96e689e3861700c746562c8a20de6db0
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-21 18:52:10 -0600

    Ignore error on docker image cleanup.

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -23,4 +23,4 @@ docker-compose -f docker-compose.prod.yml up -d
 
 # Remove untagged docker images
 echo "============ Cleanup Docker Images =============="
-docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
+docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}") || true
