663080abd97c8e10774bb3125209ca11d654b957
Shane Burkhart <shaneburkhart@gmail.com>
2017-01-23 21:05:53 -0600

    TEMP:

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -10,9 +10,11 @@ var activeCampaign = new ActiveCampaign(
 var TWITTER_CLONE_DESC = "Twitter Clone Course - Spark School";
 
 module.exports = function (app) {
-  app.post('/nodejs-tutorial/signup', function (req, res) {
+  app.post('/:type/signup', function (req, res) {
+    var type = req.params.type || 'become-a-software-dev-email-series';
     var email = req.body.email;
     var origin = req.body.origin || '/';
+    var thankYouPage = '/become-a-software-dev-email-series-thank-you';
 
     if (!/\S+@\S+.\S+/.test(email)) {
       return res.redirect([
@@ -23,17 +25,30 @@ module.exports = function (app) {
       ].join(''));
     }
 
-    var acCallback = function () { res.redirect([
-      '/take-tutorial-later-thank-you',
-      '?email=' + email,
-    ].join('')); };
     var contact = {
       'email': email,
       // Take later list
       'p[7]': '7',
     };
 
+    switch (type) {
+      case 'nodejs-tutorial':
+        contact['p[7]'] = '7';
+        thankYouPage = '/take-tutorial-later-thank-you';
+        break;
+      case 'become-a-software-dev-email-series':
+      default:
+        contact['p[8]'] = '8';
+        thankYouPage = '/become-a-software-dev-email-series-thank-you';
+        break;
+    }
+
     if (process.env.APP_ENV === 'development') contact.tags = 'development';
+
+    var acCallback = function () { res.redirect([
+      thankYouPage,
+      '?email=' + email,
+    ].join('')); };
 
     activeCampaign.api('contact/add', contact).then(acCallback, acCallback);
   });
