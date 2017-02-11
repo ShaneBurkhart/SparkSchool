d6130df257bba62901d6487b5ec2c59e852bb845
Shane Burkhart <shaneburkhart@gmail.com>
2017-02-10 22:00:58 -0600

    Add query to global variable space.

--- controllers/home.js
+++ controllers/home.js
@@ -9,14 +9,8 @@ module.exports = function (app) {
   app.get('/', function (req, res, next) {
     var gidCookie = req.cookies.ssgid;
     var priceInCents = gidUtil.PRICES[gidCookie] || gidUtil.PRICES[gidUtil.DEFAULT_PRICE_ID];
-    var query = req.query || {};
-
-    // SumoMe isn't URI encoding emails when sending them as GET vars.
-    // We need to replace spaces with plus signs.
-    if (query.email) query.email = query.email.replace(/ /g, '+');
 
     res.render('landing-pages/become-a-software-developer-without-a-college-degree-weekly-email', {
-      query: query,
       currentPath: '/become-a-software-developer-without-a-college-degree-weekly-email',
       price: Math.floor(priceInCents / 100),
       stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
@@ -28,18 +22,12 @@ module.exports = function (app) {
     var path = req.path;
     var gidCookie = req.cookies.ssgid;
     var priceInCents = gidUtil.PRICES[gidCookie] || gidUtil.PRICES[gidUtil.DEFAULT_PRICE_ID];
-    var query = req.query || {};
-
-    // SumoMe isn't URI encoding emails when sending them as GET vars.
-    // We need to replace spaces with plus signs.
-    if (query.email) query.email = query.email.replace(/ /g, '+');
 
     // Check if path has extension and 404 if does.  Landing pages don't have
     // extensions.
     if (/\/[^\/]+\.[^\/]+$/.test(path)) return next(new errorUtil.PageNotFoundError(path));
 
     res.render('landing-pages' + path, {
-      query: query,
       price: Math.floor(priceInCents / 100),
       stripePublicKey: process.env.STRIPE_PUBLIC_KEY,
     }, function (err, html) {
--- middleware/template-vars.js
+++ middleware/template-vars.js
@@ -1,8 +1,17 @@
 'use strict'
 
 module.exports = function (req, res, next) {
+  var query = req.query || {};
+
+  // SumoMe isn't URI encoding emails when sending them as GET vars.
+  // We need to replace spaces with plus signs.
+  if (query.email) query.email = query.email.replace(/ /g, '+');
+
   // Add to res.locals so it's accessible from our views.
   res.locals.currentUser = req.user;
   res.locals.currentPath = req.path;
+  res.locals.currentQuery = query;
+  res.locals.currentCookies = req.cookies || {};
+
   next()
 };
