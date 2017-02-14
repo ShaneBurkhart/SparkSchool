51954e8c07529569617812b37a725db47c5b0c8b
Shane Burkhart <shaneburkhart@gmail.com>
2017-02-14 00:33:29 -0600

    Change deploy script to use pwd.

--- deploy/sixpack.sh
+++ deploy/sixpack.sh
@@ -2,6 +2,6 @@
 
 git pull origin master
 
-docker-compose -f ../sixpack/docker-compose.yml build
-docker-compose -f ../sixpack/docker-compose.yml stop
-docker-compose -f ../sixpack/docker-compose.yml up -d
+docker-compose -f $(pwd)/sixpack/docker-compose.yml build
+docker-compose -f $(pwd)/sixpack/docker-compose.yml stop
+docker-compose -f $(pwd)/sixpack/docker-compose.yml up -d
